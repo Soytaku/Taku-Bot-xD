8fca2410749c75efe1bfe778b0951c1f1782d25e
TAKU-MD <111063542+THETAKU09@users.noreply.github.com>
2022-08-15 14:08:52 -0300

    Update audio-ya-antojaron.js

--- plugins/audio-ya-antojaron.js
+++ plugins/audio-ya-antojaron.js
@@ -8,7 +8,7 @@ ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 }
 handler.customPrefix = /ya antojaron|antojarooon|antojar|/i
 handler.command = new RegExp
-handler.help = ['ʏᴀ ᴀɴᴛᴏᴊᴀʀᴏɴ']
+handler.help = ['ya antojaron']
 handler.tags = ['audios']
 handler.fail = null
 handler.exp = 100
