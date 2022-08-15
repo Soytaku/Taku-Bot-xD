908915c24df71d30f3573ef8390f3fe711aae4ff
TAKU-MD <111063542+THETAKU09@users.noreply.github.com>
2022-08-15 13:58:21 -0300

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, '🔹Adios🖐, El tiempo del bot en este grupo ha terminado 🔹')
+        await this.reply(m.chat, '*𝙶𝙾𝙾𝙳 𝙱𝚈𝙴,👋 𝙴𝙻 𝚃𝙸𝙴𝙼𝙿𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃🤖 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾 𝙷𝙰 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾 ;)*')
         await this.groupLeave(m.chat)
         chats.expired = null
     }
