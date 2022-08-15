0f490bf75a9a24c0f2b0e1e2132d1fc320fa1dc7
TAKU-MD <111063542+THETAKU09@users.noreply.github.com>
2022-08-15 13:52:54 -0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -12,10 +12,10 @@ export async function before(m, { isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.sendButton(m.chat, `*🔹¡Link de grupo detectado!🔹*${isBotAdmin ? '' : '\n\n🔹Lo siento, el bot debe ser administrador del grupo para eliminar al spamer ☑. '}`, author, ['DESACTIVAR ANTILINK❌', '/disable antilink'], m)
+        await conn.sendButton(m.chat, `*[❗] 𝙻𝙸𝙽𝙺 𝙳𝙴𝚃𝙴𝙲𝚃𝙰𝙳𝙾!!*${isBotAdmin ? '' : '\n\nʟᴏ sɪᴇɴᴛᴏ, ᴇʟ ʙᴏᴛ ᴅᴇʙᴇ sᴇʀ ᴀᴅᴍɪɴɪsᴛʀᴀᴅᴏʀ ᴅᴇʟ ɢʀᴜᴘᴏ ᴘᴀʀᴀ ᴇʟɪᴍɪɴᴀʀ ☑. '}`, author, ['✘𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺✘', '/disable antilink'], m)
         if (isBotAdmin && bot.restrict) {
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('‼Error, el propietario tiene desactivado #restrict, para activar escribe #enable restrict o #sistemas')
+        } else if (!bot.restrict) return m.reply('‼𝙴𝚛𝚛𝚘𝚛, 𝚎𝚕 𝚙𝚛𝚘𝚙𝚒𝚎𝚝𝚊𝚛𝚒𝚘 𝚝𝚒𝚎𝚗𝚎 𝚍𝚎𝚜𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘 #restrict, 𝚙𝚊𝚛𝚊 𝚊𝚌𝚝𝚒𝚟𝚊𝚛 𝚎𝚜𝚌𝚛𝚒𝚋𝚎 #enable restrict o #sistemas')
     }
     return !0
 }
