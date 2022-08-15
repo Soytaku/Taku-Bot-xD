39253d223e6d375973d28ce24f4c3dbf7bee1862
TAKU-MD <111063542+THETAKU09@users.noreply.github.com>
2022-08-15 14:37:58 -0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
-handler.help = ['ᴄʀᴇᴀᴅᴏʀ']
+handler.help = ['creador']
 handler.tags = ['main']
 
 handler.command = /^(owner|creador)$/i
