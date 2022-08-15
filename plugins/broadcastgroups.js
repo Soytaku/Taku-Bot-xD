9d17122ca31496dbfffa6ca4ae8d7e5f0225677f
TAKU-MD <111063542+THETAKU09@users.noreply.github.com>
2022-08-15 14:36:09 -0300

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -4,11 +4,11 @@ let handler = async (m, { conn, text }) => {
   let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `🔹_Enviar mensaje de difusion a ${groups.length} grupos_`, m)
-  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「🔹 Mensaje de difusion🔹」\n' + randomID(32)), true).catch(_ => _)
-  m.reply('🔹Mensaje enviado correctamente')
+  conn.reply(m.chat, `📢𝙴𝙽𝚅𝙸𝙰𝙽𝙳𝙾 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙳𝙸𝙵𝚄𝙲𝙸𝙾𝙽 𝙰 ${groups.length} 𝙶𝚁𝚄𝙿𝙾𝚂`, m)
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙳𝙸𝙵𝚄𝙲𝙸𝙾𝙽📣」\n' + randomID(32)), true).catch(_ => _)
+  m.reply('*𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴 [✔]*')
 }
-handler.help = ['ʙʀᴏᴀᴅᴄᴀꜱᴛɢʀᴏᴜᴘ <ᴍᴇɴꜱᴀᴊᴇ>', 'ʙᴄɢᴄ']
+handler.help = ['broadcastgroup <mensaje>', 'bcgc']
 handler.tags = ['owner menu']
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i
 
