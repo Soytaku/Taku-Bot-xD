d3573e83529b3eae2db5482fb9ec20726b9241ff
TAKU-MD <111063542+THETAKU09@users.noreply.github.com>
2022-08-15 14:33:38 -0300

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -4,11 +4,11 @@ let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `🔹Enviar mensaje de difusion a ${chats.length} chats_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + author + ' 🔹Mensaje de difusion  」\n' + randomID(32)), true).catch(_ => _)
-  m.reply('🔹Mensaje enviado correctamente ☑')
+  conn.reply(m.chat, `📢𝙴𝙽𝚅𝙸𝙰𝚁 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙳𝙸𝙵𝚄𝙲𝙸𝙾𝙽 𝙰  ${chats.length} 𝙲𝙷𝙰𝚃𝚂`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + author + '𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙳𝙸𝙵𝚄𝙲𝙸𝙾𝙽📢   」\n' + randomID(32)), true).catch(_ => _)
+  m.reply('*𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴 [✔]*')
 }
-handler.help = ['.ʙʀᴏᴀᴅᴄᴀꜱᴛ <ᴍᴇɴꜱᴀᴊᴇ>', 'ʙᴄ']
+handler.help = ['.broadcast <mensaje>', 'bc']
 handler.tags = ['owner menu']
 handler.command = /^(broadcast|bc)$/i
 
